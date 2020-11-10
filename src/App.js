febfb96d0055c729e73f57ecdde918dcd9f498f1
Kim Taenam <cr000@naver.com>
2020-11-10 14:00:24 +0900

    reset css 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import "./assets/css/reset.css"
 
-function App() {
-  return (
-    <div className="App">
-      자, 이제 시작해보아요
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">자, 이제 시작해보아요</div>
+    );
 }
 
 export default App;
