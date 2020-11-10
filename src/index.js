0c0412197a711415037179de38e5d0267c0418ca
Kim Taenam <cr000@naver.com>
2020-11-10 14:02:32 +0900

    store 및 Router, provider index js 반영

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {HashRouter as Router} from "react-router-dom";
+import {UserStore} from "./stores";
+import {Provider} from 'mobx-react';
+
+const stores = {
+    UserStore,
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider {...stores}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
